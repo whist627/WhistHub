e0e4cb57b51b158bf5ac0a0d9a32c4c2644ed48d
LyricArc72030 <117631755+LyricArc72030@users.noreply.github.com>
2023-08-14 13:01:20 +0800

    Update Games.lua

--- Main/Games.lua
+++ Main/Games.lua
@@ -1,14 +1,14 @@
 local games = {
-    -- Slap Battles (Killstreak Only)
-    [11520107397] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
     -- Slap Battles
     [6403373529] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
     -- Slap Battles (No Oneshot)
-    [9015014224] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Scripts/Slap-Battles.lua', true))()"
+    [9015014224] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
+    -- Slap Battles (Killstreak Only)
+    [11520107397] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Scripts/Slap-Battles.lua', true))()"
 }
 
-if not games[game.GameId] == nil then
-    loadstring(games[tonumber(game.GameId)])()
+if not games[game.PlaceId] ~= nil then
+    loadstring(games[tonumber(game.PlaceId)])()
 else
-    game.Players.LocalPlayer:Kick("Game Not Supported!")
+    game.Players.LocalPlayer:Kick("Game Not Supported! :(")
 end
