9e6d84383c986195f9245d1f43fd707e28529581
auci349 <117631755+auci349@users.noreply.github.com>
2023-08-14 22:00:43 +0800

    Update Main-Loadstring.lua

--- Main/Main-Loadstring.lua
+++ Main/Main-Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Main/Games.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/auci349/HelloHub/main/Main/Games.lua", true))()
