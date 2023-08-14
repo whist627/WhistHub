a3868d635878118614512c9441f6d47f2818c5ed
auci349 <117631755+auci349@users.noreply.github.com>
2023-08-14 22:02:54 +0800

    Update Games.lua

--- Main/Games.lua
+++ Main/Games.lua
@@ -1,12 +1,12 @@
 local games = {
     -- Slap Battles
-    [6403373529] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
+    [6403373529] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/auci349/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
     -- Slap Battles (No Oneshot)
-    [9015014224] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
+    [9015014224] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/auci349/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
     -- Slap Battles (Killstreak Only)
-    [11520107397] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
+    [11520107397] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/auci349/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
     -- Slap Royale
-    [9431156611] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/LyricArc72030/HelloHub/main/Scripts/Slap-Royale.lua', true))()"
+    [9431156611] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/auci349/HelloHub/main/Scripts/Slap-Royale.lua', true))()"
 }
 
 if games[game.PlaceId] ~= nil then
