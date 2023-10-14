b4f188d0b2262fd3bfeeb51d65b4b0932f70bfc5
whis369 <117631755+whis369@users.noreply.github.com>
2023-10-14 09:54:35 +0800

    Update Games.lua

--- Main/Games.lua
+++ Main/Games.lua
@@ -1,19 +1,19 @@
 --[[
     DO NOT COPY THIS CODE!!
 
-    PROPERTY AND OWNED BY HELLO HUB (audi421)
+    PROPERTY AND OWNED BY HELLO HUB (whis369)
 ]]
 local games = {
     -- Slap Battles
-    [6403373529] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/audi421/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
+    [6403373529] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/whis369/HelloHub/main/Scripts/Slap-Battles.lua', true))()",
     -- Slap Battles (No Oneshot)
-    [9015014224] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/audi421/HelloHub/main/Scripts/No-Oneshot.lua', true))()",
+    [9015014224] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/whis369/HelloHub/main/Scripts/No-Oneshot.lua', true))()",
     -- Slap Battles (Killstreak Only)
-    [11520107397] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/audi421/HelloHub/main/Scripts/Killstreak-Only.lua', true))()",
+    [11520107397] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/whis369/HelloHub/main/Scripts/Killstreak-Only.lua', true))()",
     -- Slap Royale
-    [9431156611] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/audi421/HelloHub/main/Scripts/Slap-Royale.lua', true))()",
+    [9431156611] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/whis369/HelloHub/main/Scripts/Slap-Royale.lua', true))()",
     -- Eternal Bob
-    [13833961666] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/audi421/HelloHub/main/Scripts/Eternal-Bob.lua', true))()"
+    [13833961666] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/whis369/HelloHub/main/Scripts/Eternal-Bob.lua', true))()"
 }
 
 if games[game.PlaceId] ~= nil then
